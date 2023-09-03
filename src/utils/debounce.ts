151c6e25593a8eb2e907a84e285aba2a57cd6d03
Lautaro Soffici <lautarosoffici@gmail.com>
2023-09-03 16:10:32 -0300

    add vitest

--- src/utils/debounce.ts
+++ src/utils/debounce.ts
@@ -1,4 +1,7 @@
-export function debounce<Params extends any[]>(func: Function, delay: number) {
+export function debounce<Params extends unknown[]>(
+  func: Function,
+  delay: number
+) {
   let timer: ReturnType<typeof setTimeout>;
 
   return function (...args: Params) {
